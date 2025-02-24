448b15912f5e805ff23b64da34c9e64128ff51a0
poseidon0z <adityaprabhu1290@gmail.com>
2025-02-25 00:03:34 +0530

    Added main page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from 'react';
 import { SiteDataProvider } from './contexts/SiteDataProvider';
+import Handler from './Handler';
 
 function App() {
-  const [selectedDate, setSelectedDate] = useState('');
-
   return (
     <SiteDataProvider>
-      <div className="p-4 w-80 border rounded-lg shadow-md flex flex-col items-center gap-4">
-        <label className="text-gray-700 font-medium">Select Date & Time</label>
-        <input
-          type="datetime-local"
-          value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-          className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-red-400"
-        />
-      </div>
+      <Handler></Handler>
     </SiteDataProvider>
   );
 }
---
+++ src/Handler.tsx
@@ -0,0 +1,50 @@
+// import SettingsIcon from './assets/settings.svg';
+import SettingsIcon from './assets/settings.svg?react';
+
+type ThemeColors = {
+  primary: string;
+  secondary: string;
+};
+
+const themeMap: Record<number, ThemeColors> = {
+  0: { primary: '#F0EDCC', secondary: '#02343F' },
+  1: { primary: '#F9EDED', secondary: '#3F418D' },
+  2: { primary: '#F8EE00', secondary: '#111A24' },
+  3: { primary: '#EF6B6E', secondary: '#F7ED7E' },
+  4: { primary: '#FCF7F7', secondary: '#9A0001' },
+  5: { primary: '#D3C5E5', secondary: '#735DA5' },
+  6: { primary: '#FFF2D7', secondary: '#F98866' },
+  7: { primary: '#C2DFE5', secondary: '#65A4AC' },
+  8: { primary: '#F1F1F2', secondary: '#1995AD' },
+  9: { primary: '#F1D3B2', secondary: '#46211A' },
+};
+
+function switchTheme(input: number): ThemeColors | null {
+  return themeMap[input] || null;
+}
+
+function Handler() {
+  const testStuff = [0, 0, 0];
+  const colours: ThemeColors | null = switchTheme(testStuff[0]);
+
+  return (
+    <div
+      className="w-screen h-screen flex flex-col"
+      style={{ backgroundColor: colours?.primary }}
+    >
+      <SettingsIcon
+        className="absolute right-4 top-4 w-8 sm:right-8 sm:top-8 sm:w-12"
+        style={{ fill: colours?.secondary }}
+      />
+
+      <div
+        className="text-xs sm:text-sm absolute right-4 bottom-4"
+        style={{ color: colours?.secondary }}
+      >
+        A project by Poseidon0z and Azaken
+      </div>
+    </div>
+  );
+}
+
+export default Handler;
