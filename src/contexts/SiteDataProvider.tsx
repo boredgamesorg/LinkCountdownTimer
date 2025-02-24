5c4cf2936319fc9f48ebafd37e5cb36db339f9ac
poseidon0z <adityaprabhu1290@gmail.com>
2025-02-25 01:52:05 +0530

    Added default values to contexts

--- src/contexts/SiteDataProvider.tsx
+++ src/contexts/SiteDataProvider.tsx
@@ -6,9 +6,9 @@ interface MyProviderProps {
 }
 
 export const SiteDataProvider = ({ children }: MyProviderProps) => {
-  const [heading, setHeading] = useState<string>();
-  const [timeStamp, setTimeStamp] = useState<number>();
-  const [theme, setTheme] = useState<Theme>();
+  const [heading, setHeading] = useState<string>('');
+  const [timeStamp, setTimeStamp] = useState<number>(0);
+  const [theme, setTheme] = useState<Theme>({ font: 0, color: 0, design: 0 });
 
   return (
     <SiteData.Provider
