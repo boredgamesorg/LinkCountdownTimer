09dfc401adf58709bdf61524da2f75467ddffbb9
Azaken1248 <suvkrm@gmail.com>
2025-02-25 16:50:16 +0530

    Merge Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { SiteDataProvider } from "./contexts/SiteDataProvider";
+import { BrowserRouter } from "react-router-dom";
 import Handler from "./Handler";
 import { ToastContainer } from "react-toastify";
-=======
-import { BrowserRouter } from 'react-router-dom';
-import Handler from './Handler';
-import { ToastContainer } from 'react-toastify';
->>>>>>> 304ddf39f7b5fdd80789f97917a10b6786dc793b
 
 function App() {
   return (
